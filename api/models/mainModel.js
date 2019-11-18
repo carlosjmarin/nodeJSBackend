8fdd26a6602466f941d428280fdae874d41276ff
zistieff <zistieff@tiu.edu>
2019-11-17 18:35:28 -0600

    New schema

--- api/models/mainModel.js
+++ api/models/mainModel.js
@@ -23,4 +23,87 @@ var UserSchema = new Schema({
   }
 });
 
+var StudentSchema = new Schema({
+  firstName: {
+    type: String,
+    required: 'Kindly enter your firstName'
+  },
+  lastName: {
+    type: String,
+    required: 'Kindly enter your lastName'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  trinityID: {
+    type: Number,
+    required: 'put your trinity id here'
+  }
+});
+
+var AdminSchema = new Schema({
+  firstName: {
+    type: String,
+    required: 'Kindly enter your firstName'
+  },
+  lastName: {
+    type: String,
+    required: 'Kindly enter your lastName'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  trinityID: {
+    type: Number,
+    required: 'put your trinity id here'
+  },
+  facultyTF: {
+    type: Boolean
+  }
+});
+
+var EventSchema = new Schema({
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  Club: {
+    type: Boolean
+  },
+  Sport: {
+    type: Boolean
+  }
+});
+
+var HawkinsSchema = new Schema({
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  Meal: {
+    type: Boolean
+  },
+  FRESH: {
+    type: Boolean
+  }
+});
+
+var SportsSchema = new Schema({
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  Basketball: {
+    type: Boolean
+  },
+  Football: {
+    type: Boolean
+  },
+  Baseball: {
+    type: Boolean
+  }
+});
+
 module.exports = mongoose.model('Users', UserSchema);
